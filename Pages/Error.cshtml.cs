676ac8b2a976743f2590f782c8b549d5b7ed2523
Zach Morris <zmorris@covenanttransport.com>
2018-12-04 12:43:09 -0500

    Recreated dotnet angular application and installed latest angular in separate ClientApp.

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace routine.ly.Pages
+namespace routine_ly.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace routine.ly
+namespace routine_ly
 {
     public class Program
     {
