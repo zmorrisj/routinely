404564fdf52e7fb5d326be3cd3fe59068e543955
Zach Morris <zmorris@covenanttransport.com>
2018-12-04 13:06:18 -0500

    Created basic stylesheet setup and reorganized a few base application files.

--- ClientApp/e2e/src/app.e2e-spec.ts
+++ ClientApp/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to routine-ly!');
+    expect(page.getTitleText()).toEqual('Welcome to routine.ly!');
   });
 });
