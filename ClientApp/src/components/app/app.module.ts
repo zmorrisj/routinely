5aee4c44f14af73eb9b333dc7e150d6eccf97a2e
zmorrisj <zmorris.j@gmail.com>
2019-04-06 17:56:50 -0400

    Created new cooking-skill.component separate from the app.component.

--- ClientApp/src/components/app/app.module.ts
+++ ClientApp/src/components/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from '../../app-routing.module';
 import { AppComponent } from './app.component';
 import { CookingSkillService } from 'src/services/cooking-skill.service';
+import { CookingSkillComponent } from '../cooking-skill.component/cooking-skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CookingSkillComponent
   ],
   imports: [
     BrowserModule,
